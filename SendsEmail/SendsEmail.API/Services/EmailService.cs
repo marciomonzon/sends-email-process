fe4c430de5a6442b66dcbf51d0ffddc0105c8c6e
monzondeveloper <monzondeveloper@gmail.com>
2025-01-07 10:49:58 -0300

    api controller and rabbitmq implementation method

--- SendsEmail/SendsEmail.API/Services/EmailService.cs
+++ SendsEmail/SendsEmail.API/Services/EmailService.cs
@@ -1,5 +1,8 @@
-﻿using SendsEmail.API.Dtos;
+﻿using RabbitMQ.Client;
+using SendsEmail.API.Dtos;
 using SendsEmail.API.Services.Interfaces;
+using System.Text;
+using System.Text.Json;
 
 namespace SendsEmail.API.Services
 {
@@ -7,7 +10,32 @@ namespace SendsEmail.API.Services
     {
         public async Task<bool> NotifyToSendEmailAsync(EmailDto dto)
         {
-            return false;
+            try
+            {
+                var factory = new ConnectionFactory { HostName = "localhost" };
+                using var connection = await factory.CreateConnectionAsync();
+                using var channel = await connection.CreateChannelAsync();
+
+                await channel.QueueDeclareAsync(queue: "email",
+                                                durable: false,
+                                                exclusive: false,
+                                                autoDelete: false,
+                                                arguments: null);
+
+                var messageJson = JsonSerializer.Serialize(dto);
+
+                var body = Encoding.UTF8.GetBytes(messageJson);
+
+                await channel.BasicPublishAsync(exchange: string.Empty,
+                                                routingKey: "email",
+                                                body: body);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
