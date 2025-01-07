32ab9e262577fb87d7f95f23d89d3501e80088f0
monzondeveloper <monzondeveloper@gmail.com>
2025-01-07 10:12:36 -0300

    first commit of the api with the email service and its interface

---
+++ SendsEmail/SendsEmail.API/Services/EmailService.cs
@@ -0,0 +1,51 @@
+﻿using SendsEmail.API.Services.Interfaces;
+using System.Net.Mail;
+using System.Net;
+
+namespace SendsEmail.API.Services
+{
+    public class EmailService : IEmailService
+    {
+        private string? _senderEmail;
+        private string? _senderPassword;
+        private string? _smtpServer;
+        private int _smtpPort;
+
+        public void SetConfiguration()
+        {
+            _senderEmail = "email@email.com";
+            _senderPassword = "password";
+            _smtpServer = "localhost";
+            _smtpPort = 587;
+        }
+
+        public async Task<bool> SendEmailAsync(string subject, string body, string recipientEmail)
+        {
+            try
+            {
+                using (var smtpClient = new SmtpClient(_smtpServer, _smtpPort))
+                {
+                    smtpClient.Credentials = new NetworkCredential(_senderEmail, _senderPassword);
+                    smtpClient.EnableSsl = true;
+
+                    var mailMessage = new MailMessage
+                    {
+                        From = new MailAddress(_senderEmail!),
+                        Subject = subject,
+                        Body = body,
+                        IsBodyHtml = true
+                    };
+
+                    mailMessage.To.Add(recipientEmail);
+
+                    await smtpClient.SendMailAsync(mailMessage);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ SendsEmail/SendsEmail.API/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿namespace SendsEmail.API.Services.Interfaces
+{
+    public interface IEmailService
+    {
+        void SetConfiguration();
+        Task<bool> SendEmailAsync(string subject, string body, string recipientEmail);
+    }
+}
